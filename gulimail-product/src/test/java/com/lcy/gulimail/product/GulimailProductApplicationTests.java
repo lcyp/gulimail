59456b7a19337a4e374a2c76f00776f8bcb9f3a7
lcyp <2089117474@qq.com>
2025-05-18 19:37:35 +0800

    第一次提交

---
+++ gulimail-product/src/test/java/com/lcy/gulimail/product/GulimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcy.gulimail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
